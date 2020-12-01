5e1cd2dab83026f6261824d00cf67abd3d4602a1
Reekraj Roy <55712612+REEKRAJROY@users.noreply.github.com>
2020-12-02 00:49:13 +0530

    first commit

---
+++ lib/screens/launch_screen.dart
@@ -0,0 +1,76 @@
+
+import 'package:flutter/material.dart';
+
+import 'package:provider/provider.dart';
+
+
+class LaunchScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<AuthenticationState>(
+      builder: (builder, authState, child) {
+        print(authState.authStatus);
+        gotoHomeScreen(context, authState);
+        return Container(
+          width: 400,
+          margin: EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(top: 100),
+                child: Text(
+                  kAppName,
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blueAccent,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 200.0,
+              ),
+              if (authState.authStatus == kAuthLoading)
+                Text(
+                  'loading...',
+                  style: TextStyle(fontSize: 12.0),
+                ),
+              if (authState.authStatus == null ||
+                  authState.authStatus == kAuthError)
+                Container(
+                  child: Column(
+                    children: <Widget>[
+                      LoginButton(
+                        label: 'Google Sign In',
+                        onPressed: () =>
+                            signIn(context, kAuthSignInGoogle, authState),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      LoginButton(
+                        label: 'Anonymous Sign In',
+                        onPressed: () =>
+                            signIn(context, kAuthSignInAnonymous, authState),
+                      ),
+                    ],
+                  ),
+                ),
+              if (authState.authStatus == kAuthError)
+                Text(
+                  'Error...',
+                  style: TextStyle(fontSize: 12.0, color: Colors.redAccent),
+                ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  void signIn(context, String service, AuthenticationState authState) {
+    
+    authState.login(serviceName: service);
+  }
+}
